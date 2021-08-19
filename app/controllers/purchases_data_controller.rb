592c57d02e43d617180d4cb9398c0b97a6ffc7c5
Claudinei Perboni <cperbony@gmail.com>
2021-08-19 00:22:29 -0300

    update controller pagination

--- app/controllers/purchases_data_controller.rb
+++ app/controllers/purchases_data_controller.rb
@@ -1,9 +1,26 @@
 class PurchasesDataController < ApplicationController
-  def index
-    @data = PurchaseData.all
+  before_action :process_all_data, :load_data, only: %i[index]
 
-    render json: {
-      data: @data.map(&:to_app),
-    }
+  def index
+    @data_purchases = @data_purchases.order(created_at: :desc).page(params[:page])
+    @data_purchases.map(&:to_app)
+
+    # Used to render infos json
+    # render json: { data: @data.map(&:to_app) }
+  end
+
+  private
+
+  # When index load, process_data is called throught rails before_action to load new data inserted in 'base_teste.txt'
+  def process_all_data
+    PurchaseData.new.process_data
+  end
+
+  def load_data
+    @data_purchases = PurchaseData.where.not(cpf: nil).all
+  end
+
+  def permitted_params
+    params.permit(%i[page])
   end
 end
